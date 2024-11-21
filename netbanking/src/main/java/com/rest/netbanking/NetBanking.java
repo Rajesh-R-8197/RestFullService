70d43b1c8aa5f8005b4631cdd423c395955fc7ee
Rajesh-R-8197 <rajeshraju8197@gmail.com>
2024-11-21 15:54:42 +0530

    update NetBanking class

--- netbanking/src/main/java/com/rest/netbanking/NetBanking.java
+++ netbanking/src/main/java/com/rest/netbanking/NetBanking.java
@@ -1,4 +1,21 @@
 package com.rest.netbanking;
 
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+
+import java.security.SecureRandom;
+
+@Path("/account")
 public class NetBanking {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public double getBalance(@QueryParam("accountNo") String accountNo) {
+        SecureRandom random = new SecureRandom();
+        double balance = random.nextDouble(239999);
+        return balance;
+    }
 }
